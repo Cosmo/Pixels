7b4665712f2c5026ddd70255854d41400f2f346a
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-17 12:15:26 +0100

    Add flag to fill boxes

--- Sources/Pixels/Shapes/Boxes.swift
+++ Sources/Pixels/Shapes/Boxes.swift
@@ -1,21 +1,27 @@
 extension Pixels {
-    public mutating func drawBox(x: Int, y: Int, width: Int, height: Int, radius: Int = 0, color: ColorDepth = ColorDepth.max, dotted isDottedEnabled: Bool = false, brushSize: Int = 1) {
+    public mutating func drawBox(x: Int, y: Int, width: Int, height: Int, radius: Int = 0, color: ColorDepth = ColorDepth.max, dotted isDottedEnabled: Bool = false, brushSize: Int = 1, filled isFilled: Bool = false) {
         var radius = radius
         
         if radius > width / 2 || radius > height / 2 {
             radius = min(width, height) / 2
         }
         
-        drawHorizontalLine(x: x + radius, y: y, width: width - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
-        drawHorizontalLine(x: x + radius, y: y + height - 1, width: width - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
-        drawVerticalLine(x: x, y: y + radius, height: height - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
-        drawVerticalLine(x: x + width - 1, y: y + radius, height: height - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
-        
-        if radius > 0 {
-            drawCircle(xm: x + radius, ym: y + radius, radius: radius, color: color, corners: .leftTop, dotted: isDottedEnabled, brushSize: brushSize)
-            drawCircle(xm: x + width - radius - 1, ym: y + radius, radius: radius, color: color, corners: .rightTop, dotted: isDottedEnabled, brushSize: brushSize)
-            drawCircle(xm: x + radius, ym: y + height - radius - 1, radius: radius, color: color, corners: .leftBottom, dotted: isDottedEnabled, brushSize: brushSize)
-            drawCircle(xm: x + width - radius - 1, ym: y + height - radius - 1, radius: radius, color: color, corners: .rightBottom, dotted: isDottedEnabled, brushSize: brushSize)
+        if isFilled {
+            for lineIndex in 0..<height {
+                drawHorizontalLine(x: x, y: y + lineIndex, width: width, color: color, dotted: isDottedEnabled, brushSize: brushSize)
+            }
+        } else {
+            drawHorizontalLine(x: x + radius, y: y, width: width - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
+            drawHorizontalLine(x: x + radius, y: y + height - 1, width: width - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
+            drawVerticalLine(x: x, y: y + radius, height: height - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
+            drawVerticalLine(x: x + width - 1, y: y + radius, height: height - radius * 2, color: color, dotted: isDottedEnabled, brushSize: brushSize)
+            
+            if radius > 0 {
+                drawCircle(xm: x + radius, ym: y + radius, radius: radius, color: color, corners: .leftTop, dotted: isDottedEnabled, brushSize: brushSize)
+                drawCircle(xm: x + width - radius - 1, ym: y + radius, radius: radius, color: color, corners: .rightTop, dotted: isDottedEnabled, brushSize: brushSize)
+                drawCircle(xm: x + radius, ym: y + height - radius - 1, radius: radius, color: color, corners: .leftBottom, dotted: isDottedEnabled, brushSize: brushSize)
+                drawCircle(xm: x + width - radius - 1, ym: y + height - radius - 1, radius: radius, color: color, corners: .rightBottom, dotted: isDottedEnabled, brushSize: brushSize)
+            }
         }
     }
 }
