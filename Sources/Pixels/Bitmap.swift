f114458eaf965ffda34af5afb42ec8da6b9352b8
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-11-30 15:53:35 +0100

    Add color to bitmap rendering

--- Sources/Pixels/Bitmap.swift
+++ Sources/Pixels/Bitmap.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Pixels {
-    public mutating func drawBitmap(bytes: [UInt8], x: Int, y: Int, width: Int, height: Int) {
+    public mutating func drawBitmap(bytes: [UInt8], x: Int, y: Int, width: Int, height: Int, color: ColorDepth = ColorDepth.min) {
         var line = 0
         for (index, byte) in bytes.enumerated() {
             for bitIndex in 0..<8 {
@@ -16,7 +16,8 @@ extension Pixels {
                 if shouldDraw {
                     let pixelIndex = (bitIndex + (index * 8)) % width
                     setPixel(x: pixelIndex + x,
-                             y: line + y)
+                             y: line + y,
+                             color: color)
                 }
             }
             if index > 0 && index % (width / 8) == 0 {
